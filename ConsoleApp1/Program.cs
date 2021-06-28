39d380dc8cb7fb0afbb982ac861930831f5fe888
Mason Oltermann <m.oltermann@gmail.com>
2021-06-28 15:36:55 -0500

    Final Copy

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -9,30 +10,30 @@ namespace ConsoleApp1
         /// </summary>
         private class MenuItem
         {
-            private readonly int price;
-            private readonly string description;
-            private readonly int category;
-            private readonly DateTime dateAdded;
 
-            public MenuItem(int Price, string description, int category, DateTime DateAdded)
+            public int Price { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime DateAdded { get; set; }
+            public string DishName { get; set; }
+
+            public MenuItem(string DishNameNR, int PriceNR, string DescriptionNR, string CategoryNR, DateTime DateAddedNR)
             {
-                this.Price = this.Price;
-                Description = Description;
-                Category = Category;
-                DateAdded = DateAdded;
+                DishName = DishNameNR;
+                Price = PriceNR;
+                Description = DescriptionNR;
+                Category = CategoryNR;
+                DateAdded = DateAddedNR;
             }
 
-            private int Price { get; set; }
-            private string Description { get; set; }
-            private int Category { get; set; }
-            private DateTime DateAdded { get; set; }
-            
-          
+
+
         }
 
         private static class Menu
         { 
-        
+        //create lists, for loop, writeline
+
         }
         
         
@@ -43,20 +44,31 @@ namespace ConsoleApp1
         
         static void Main(string[] args)
         {
-            MenuItem BabaGanoush = new MenuItem(25, "An acidic and refreshing dip created from roasted eggplant" +
-                "bright lemon, and fresh herbs. Served with pita", 1, new DateTime(2021,06,28));
-            MenuItem Hummus = new MenuItem(23, "An earthy blend of hearty chickpeas, zesty citrus, and exotic spices" +
-                "served with pita", 1, new DateTime(2021, 06, 28));
-            MenuItem TofuCurry = new MenuItem(36, "Marinated tofu simmered alongside locally sourced produce. Served" +
-                " with jasmine rice. Vegan", 2, new DateTime(2021, 06, 28));
-            MenuItem Salad = new MenuItem(33, "This elevated salad contains organic rahubarb in tandem with" +
-                " rainbow radishes atop a kale bed and lightly dressed with raspberry vinegrette. ", 2, new
+            List<MenuItem> MenuList = new List<MenuItem> { };
+            MenuItem BabaGanoush = new MenuItem("Baba Ganoush", 25, "An acidic and refreshing dip created from roasted eggplant" +
+                "bright lemon, and fresh herbs. Served with pita", "App", new DateTime(2021,06,28));
+            MenuItem Hummus = new MenuItem("Hummus", 23, "An earthy blend of hearty chickpeas, zesty citrus, and exotic spices" +
+                "served with pita", "App", new DateTime(2021, 06, 28));
+            MenuItem TofuCurry = new MenuItem("Tofu Curry", 36, "Marinated tofu simmered alongside locally sourced produce. Served" +
+                " with jasmine rice. Vegan", "Entre", new DateTime(2021, 06, 28));
+            MenuItem Salad = new MenuItem("Salad", 33, "This elevated salad contains organic rahubarb in tandem with" +
+                " rainbow radishes atop a kale bed and lightly dressed with raspberry vinegrette. ", "entre", new
                 DateTime(2021,06,28));
-            MenuItem StrawberriesRomanoff = new MenuItem(18, "this dish marries farm fresh straberries and creme, all" +
-                "with earthy spices to bring us back down.", 3, new DateTime(2021, 06, 28));
-            MenuItem Baklava = new MenuItem(20, "Wafer-thin filo dough layered with localhoney and Iranian pistachios.",
-                3, new DateTime(2021, 06, 28));
-
+            MenuItem StrawberriesRomanoff = new MenuItem("Strawberries Romanoff", 18, "this dish marries farm fresh straberries and creme, all" +
+                "with earthy spices to bring us back down.", "Des", new DateTime(2021, 06, 28));
+            MenuItem Baklava = new MenuItem("Baklava", 20, "Wafer-thin filo dough layered with localhoney and Iranian pistachios.",
+                "des", new DateTime(2021, 06, 28));
+            MenuList.Add(BabaGanoush);
+            MenuList.Add(Hummus);
+            MenuList.Add(TofuCurry);
+            MenuList.Add(Salad);
+            MenuList.Add(StrawberriesRomanoff);
+            MenuList.Add(Baklava);
+            foreach (MenuItem item in MenuList)
+            {
+                Console.WriteLine($"{item.DishName} {item.Category} {item.Description} {item.Price}");
+            }
+            ;
             
         }
     }
