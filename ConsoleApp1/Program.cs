29b0cf00d197245074e0137518e342e6d763b1d8
Mason Oltermann <m.oltermann@gmail.com>
2021-06-28 13:44:34 -0500

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static class MenuItem
+        {
+            private static int Price { get; set; }
+            private static string Description;
+            private static int Category;
+            private static DateTime DateAdded;
+          
+        }
+
+        private static class Menu
+        { 
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
